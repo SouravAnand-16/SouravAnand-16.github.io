809bab08b742d96a232e5bffa7549ac840765980
Sourav Anand <sauravanand324@gmail.com>
2024-02-16 18:13:25 +0530

    done
    done

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    const menu = document.querySelector(".menu-link");
+    const icon = document.querySelector(".humberg-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
